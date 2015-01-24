3ea029cfc24ad5fd7a8eadb02a5288bb0afdd9d9
danilaplee <fiend.of.sound@gmail.com>
2015-01-24 19:44:43 +0300

    setting up server

--- hoster/app.js
+++ hoster/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var connect = require('connect');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -20,6 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(connect.compress());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
