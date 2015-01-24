5ed70d77592491d85bbcfdd004db010c6e45d51e
danilaplee <fiend.of.sound@gmail.com>
2015-01-24 19:47:18 +0300

    setting up server

--- hoster/app.js
+++ hoster/app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(connect.compress());
+app.use(express.compress());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
